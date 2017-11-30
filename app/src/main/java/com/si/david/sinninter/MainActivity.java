1c6e5d491c65683aeeb5e1fbcdb34ceca07415e7
Jakob Fuchs <34133848+j-fuchs@users.noreply.github.com>
2017-11-30 21:00:56 +0100

    fixed crash

--- app/src/main/java/com/si/david/sinninter/MainActivity.java
+++ app/src/main/java/com/si/david/sinninter/MainActivity.java
@@ -1,5 +1,6 @@
 package com.si.david.sinninter;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,13 +16,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener,
+public class MainActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener,
         MapFragment.OnFragmentInteractionListener,
-        UnlockFrag.OnFragmentInteractionListener{ {
+        UnlockFrag.OnFragmentInteractionListener
+        {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
